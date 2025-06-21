239b50e6b08f5f4ea7c331b052415e72ba7b306b
Bou-Chai <TyseerShahriar@gmail.com>
2025-06-20 21:46:29 -0600

    Started and almost finished (maybe) title functionality

--- include/Tables/Table.h
+++ include/Tables/Table.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <stdexcept>
 #include <unordered_map>
+#include <fstream>
 #include "ColumnBase.h"
 #include "Tables/Column.h"
 
@@ -28,9 +29,11 @@ namespace tables {
 
         // Destructor
         ~Table() {
+            std::cout << "Table dtor:" << "\n";
             for (ColumnBase* columnP : table) {
                 delete columnP;
             }
+            std::cout << "End Table dtor:" << "\n";
         }
 
         // Function to access table element
@@ -38,13 +41,20 @@ namespace tables {
         T& at(int col, int row) {
             return this->col<T>(col).row(row);
         }
+
+        // Function to access table element using title
+        template <typename T>
+        T& at(std::string title, int row) {
+            return this->col<T>(title).row(row);
+        }
 /*
         // Function to access table column using square brackets
         template <typename T>
         Column<T>& operator[](int col) {
             return this->col<T>(col);
         }
-*/
+*/      
+        // Function to access column
         template <typename T>
         Column<T>& col(int col) {
             Column<T>* columnPtr = dynamic_cast<Column<T>*>(table.at(col));
@@ -52,22 +62,76 @@ namespace tables {
                 throw std::invalid_argument("Table::col: Invalid template type used to retrieve column");
             }
             return *columnPtr;
-        }   
+        }
+
+        // Function to access column using title
+        template <typename T>
+        Column<T>& col(std::string title) {
+            try {
+                Column<T>* columnPtr = dynamic_cast<Column<T>*>(columnMap.at(title));
+                if (columnPtr == nullptr) {
+                    throw std::invalid_argument("Table::col: Invalid template type used to retrieve column");
+                }
+                return *columnPtr;
+            } catch (std::out_of_range) {
+                throw std::out_of_range("Table::col: No column with title " + title);
+            }
+        }
         
+        // Function to add column
         template <typename T>
         void addColumn(Column<T> column) {
             table.push_back(new Column(column));
         }
 
+        // Function to add column with title
+        template <typename T>
+        void addColumn(std::string title, Column<T> column) {
+            Column<T>* newColumn = new Column(column);
+            table.push_back(newColumn);
+            columnMap[title] = newColumn;
+        }
+
+        // Function to remove column using index
         void removeCol(int col) {
-            // TODO: make sure object is deleted
+            std::unordered_map<std::string, ColumnBase*>::iterator it;
+            for (it = columnMap.begin(); it != columnMap.end(); ++it) {
+                if (it->second == table[col]) {
+                    columnMap.erase(it);
+                    break;
+                }
+            }
             delete table[col];
             table.erase(table.begin() + col);
         }
 
+        // Function to remove column using title
+        void removeCol(std::string title) {
+            std::vector<ColumnBase*>::iterator it;
+            for (it = table.begin(); it != table.end(); ++it) {
+                try {
+                    if (*it == columnMap.at(title)) {
+                        table.erase(it);
+                        break;
+                    }
+                } catch (std::out_of_range) {
+                    throw std::out_of_range("Table::removeCol: No column with title " + title);
+                }
+            }
+            delete columnMap.at(title);
+            columnMap.erase(title);
+        }
+
+        // Funciton to return sum of column using index
         template <typename T>
         T sum(int col) {
             return this->col<T>(col).sum();
+        }
+
+        // Function to return sum of column using title
+        template <typename T>
+        T sum(std::string title) {
+            return this->col<T>(title).sum();
         }
 
         int getHeight() {
@@ -94,8 +158,38 @@ namespace tables {
 
         }
 
-        double sumCol(int col) {
+        void loadCSV(std::string fileName) {
+            /*
+            std::vector<std::vector<double>>* table = new std::vector<std::vector<double>>;
+            std::ifstream file(fileName);
+            std::string line;
 
+
+
+            if (file.is_open()) {
+                for (int i = 0; getline(file, line); i++) {
+                    table->push_back({});
+                    std::string element = "";
+                    for (int j = 0; j < line.size(); j++) {
+                        if (std::isdigit(line[j]) || line[j] == '.') {
+                            element += line[j];
+                        } else if (line[j] == ',') {
+                            std::cout << element << "\n";
+                            table->at(i).push_back(std::stof(element));
+                            element = "";
+                        } else {
+                            break;
+                        }
+                    }
+                    if (element != "") {
+                        table->at(i).push_back(std::stof(element));
+                    }
+                }
+                file.close();
+            } else {
+                std::cout << "Failed to open file" << "\n";
+            }
+                */
         }
 
         void print() {
@@ -106,8 +200,7 @@ namespace tables {
 
     private:
         std::vector<ColumnBase*> table;
-        std::vector<bool> numerical;
-        std::unordered_map<std::string, int> columnNames;
+        std::unordered_map<std::string, ColumnBase*> columnMap;
     };
 }
 
--- tests/Test.cpp
+++ tests/Test.cpp
@@ -12,12 +12,12 @@ int main() {
     tables::ColumnBase* col1P;
     tables::ColumnBase* col2P;
     tables::Table table;
+    
 
     //col1P = (tables::ColumnBase*) &col1;
 
     //col1P = new tables::NumericalColumn<double>;
     
-
     col1.add(1);
     col1.add(2);
     col2.add(3.0);
@@ -25,6 +25,23 @@ int main() {
     col3.add("Tim");
     col3.add("Oborn");
 
+    //table.addColumn<int>("Ints", col1);
+    //table.addColumn<float>(col2);
+    table.addColumn<std::string>("Strings", col3);
+    table.col<std::string>("Stringss");
+
+    table.print();
+/*
+    table.col<std::string>("Strings").row(1) = "bo";
+    table.print();
+    
+    table.at<std::string>("Strings", 1) = "bro";
+    //table.removeCol("Ints");
+    table.print();
+
+    std::cout << table.col<int>("Ints").sum() << "\n";
+    std::cout << table.sum<float>("Floats") << "\n";
+/*
     table.addColumn(col1);
     table.addColumn(col2);
     table.addColumn(col3);
@@ -39,6 +56,6 @@ int main() {
 
     std::cout << table.col<int>(0).sum() << "\n";
     std::cout << table.sum<float>(1) << "\n";
-    //delete col1P;
+ */   //delete col1P;
     return 0;
 }
